cff28973d5b7e02fa6b5ce26608f984140260e8b
Stanley Osagie Ramos <sagiecyber@gmail.com>
2022-11-09 16:39:53 +0100

    bubble sort algorithm

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
